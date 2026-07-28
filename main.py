39a68ec1966027f009f34c5b1da5f35f158e3178
Oruche Daniel <novumdude@gmail.com>
2026-07-28 05:26:50 +0100

    Added conversion to binary as default behaviour

--- main.py
+++ main.py
@@ -3,9 +3,21 @@ from argparse import ArgumentParser
 def main() -> None:
     parser = ArgumentParser(prog="dtb-converter", description="Convert decimal to binary")
     
-    parser.add_argument("number")
+    parser.add_argument("number", type=int)
 
     args = parser.parse_args()
+
+    out = convert_to_binary(args.number)
+    print(out)
+
+def convert_to_binary(num:int):
+    rem = 0
+    res = ""
+    while num > 0:
+        rem = num % 2
+        num = num // 2
+        res = str(rem) + res
+    return res
 
 if __name__ == "__main__":
     main()
